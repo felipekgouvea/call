99eca3f5368540c0ee98fc3ae8b2c79c5b4cc1f0
felipekgouvea <felipekinupesg@hotmail.com>
2024-06-01 13:31:55 -0300

    feat: add Stepper component

--- app/_components/stepper.tsx
+++ app/_components/stepper.tsx
@@ -1,14 +1,36 @@
-function Stepper() {
+import { cn } from '../_lib/utils'
+
+interface StepperProps {
+  step: number
+  className?: string
+}
+
+function Stepper({ step, className }: StepperProps) {
   return (
     <div className="flex w-full flex-col gap-2">
       <span className="text-xs font-normal text-muted-foreground">
-        Passo 1 de 4
+        Passo {step} de 4
       </span>
       <div className="flex gap-1">
-        <div className="h-1 w-28 bg-muted" />
-        <div className="h-1 w-28 bg-muted-foreground opacity-55" />
-        <div className="h-1 w-28 bg-muted-foreground opacity-55" />
-        <div className="h-1 w-28 bg-muted-foreground opacity-55" />
+        <div className={cn('h-1 w-28 bg-muted', step === 1 ? className : '')} />
+        <div
+          className={cn(
+            'h-1 w-28 bg-muted-foreground',
+            step === 2 || step === 3 || step === 4 ? className : '',
+          )}
+        />
+        <div
+          className={cn(
+            'h-1 w-28 bg-muted-foreground',
+            step === 3 || step === 4 ? className : '',
+          )}
+        />
+        <div
+          className={cn(
+            'h-1 w-28 bg-muted-foreground',
+            step === 4 ? className : '',
+          )}
+        />
       </div>
     </div>
   )
