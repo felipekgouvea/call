8669a3db8ec775a496ad3e0f104facc3165ae339
felipekgouvea <felipekinupesg@hotmail.com>
2024-06-01 13:31:15 -0300

    chore: setup next oauth

---
+++ app/_lib/auth.ts
@@ -0,0 +1,28 @@
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { AuthOptions } from 'next-auth'
+import { Adapter } from 'next-auth/adapters'
+import GoogleProvider from 'next-auth/providers/google'
+
+import { prisma } from '@/app/_lib/prisma'
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user = { ...session.user, id: user.id } as {
+        id: string
+        name: string
+        email: string
+      }
+
+      return session
+    },
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
+}
